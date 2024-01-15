937280906f2155f63b06c6e36ce10c0d2826bd6d
Henry Yun <henrysungyun@gmail.com>
2024-01-15 14:22:27 -0800

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from 'react-icons/bs';
+
+function About() {
+    return (
+        <div className="about-section-container">
+            <div className="about-background-image-container">
+                <img src={AboutBackground} alt="" />
+            </div>
+            <div className="about-section-image-container">
+                <img src={AboutBackgroundImage} alt="" />
+            </div>
+            <div className="about-section-text-container">
+                <p className="primary-subheading">About</p>
+                <h1 className="primary-heading">
+                    Food is an Important Part of a Balanced Diet
+                </h1>
+                <p className="primary-text">
+                    Lorem ipsum dolor sit amet consectetur. Non ticident magna non et alit. Dolor turpsi molestie dui magnis failisis at fringilla quam.
+                </p>
+                <p className="primry-text">
+                    Lorem ipsum dolor sit amet consectetur. Non ticident magna non et alit. Dolor turpsi molestie dui magnis failisis at fringilla quam.
+                </p>
+                <div className="about-buttons-container">
+                    <button className="secondary-button">Learn More</button>
+                    <button className="watch-video-button"><BsFillPlayCircleFill /> Watch Video</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
